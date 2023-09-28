471fcbb655f5ec05ec95ce2a5b65e95f3c599b74
hss-mateus <hss-mateus@pm.me>
2023-09-28 16:03:48 -0300

    Add uniqueness validation for category names

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,8 +7,12 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
 class Category < ApplicationRecord
   has_many :items, dependent: :nullify
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_184641) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "items", force: :cascade do |t|
