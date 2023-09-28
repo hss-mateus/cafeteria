e713b622a469a61fe008b3896919c5748ce82a6d
hss-mateus <hss-mateus@pm.me>
2023-09-28 16:48:21 -0300

    Setup active-storage/action-text

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 import "./controllers"
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
+import "trix"
+import "@rails/actiontext"
 
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   document.documentElement.setAttribute("data-bs-theme", "dark")
