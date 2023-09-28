8b09c23e9decaa430139a8043b5e6857dcc05ad0
hss-mateus <hss-mateus@pm.me>
2023-09-28 15:44:42 -0300

    Setup model annotations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                           :bigint           not null, primary key
+#  activation_state             :string
+#  activation_token             :string
+#  activation_token_expires_at  :datetime
+#  crypted_password             :string
+#  email                        :string
+#  name                         :string
+#  remember_me_token            :string
+#  remember_me_token_expires_at :datetime
+#  salt                         :string
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_activation_token   (activation_token)
+#  index_users_on_email              (email) UNIQUE
+#  index_users_on_remember_me_token  (remember_me_token)
+#
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
