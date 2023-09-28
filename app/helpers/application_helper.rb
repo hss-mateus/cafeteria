be243712f8fd453c7242f5d6588381e5df40276a
hss-mateus <hss-mateus@pm.me>
2023-09-28 19:46:08 -0300

    Add flash support from turbo streams

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,14 +12,14 @@ module ApplicationHelper
   end
 
   def flash_color(type)
-    case type
+    case type.to_s
     when "notice" then "primary"
     when "alert" then "warning"
     end
   end
 
   def flash_icon(type)
-    case type
+    case type.to_s
     when "notice" then "bi-info-circle-fill"
     when "alert" then "bi-exclamation-triangle-fill"
     end
@@ -27,5 +27,11 @@ module ApplicationHelper
 
   def cents_to_currency(cents)
     number_to_currency(Money.new(cents:))
+  end
+
+  def update_flash(message:, type: :notice)
+    turbo_stream.update(:flash) do
+      render("shared/flash", type:, message:)
+    end
   end
 end
