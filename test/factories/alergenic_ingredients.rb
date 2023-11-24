afb35451d222c4eb6888fc9c97daa70aece4b221
hss-mateus <hss-mateus@pm.me>
2023-11-24 16:15:20 -0300

    Annotate alergenic_ingredients

--- test/factories/alergenic_ingredients.rb
+++ test/factories/alergenic_ingredients.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: alergenic_ingredients
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :integer          not null
+#
+# Indexes
+#
+#  index_alergenic_ingredients_on_product_id  (product_id)
+#
+# Foreign Keys
+#
+#  product_id  (product_id => products.id)
+#
 FactoryBot.define do
   factory :alergenic_ingredient do
     product
