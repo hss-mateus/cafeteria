30ab576265c5439f88a6913f1dd3980086cb21c2
hss-mateus <hss-mateus@pm.me>
2023-11-27 16:42:06 -0300

    Set custom modal for turbo-confirm

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 import "./controllers"
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "@turbo-boost/streams"
 import * as bootstrap from "bootstrap"
 import "trix"
@@ -9,3 +9,18 @@ import "chartkick/chart.js"
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   document.documentElement.setAttribute("data-bs-theme", "dark")
 }
+
+Turbo.setConfirmMethod((message, element, button) => {
+  let modal = document.querySelector("#confirm")
+  let confirmButton = modal.querySelector("#confirm-button")
+
+  modal.querySelector("#confirm-text").textContent = element.dataset.turboConfirm
+
+  new bootstrap.Modal(modal).show()
+
+  return new Promise(resolve => {
+    modal.addEventListener("hide.bs.modal", e => {
+      resolve(e.explicitOriginalTarget == confirmButton)
+    }, { once: true })
+  })
+})
